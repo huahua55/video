790e7de66bb2f76acf9414cfdbc4e9002f7270e9
dongyule <dongyule@zuinianqing.com>
2020-03-18 14:16:02 +0800

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 define('APP_PATH', dirname(__FILE__) . '/app');
 
-define('IPHP_PATH', dirname(__FILE__) . '/iphp');
+define('IPHP_PATH', '/data/lib/iphp');
 require_once(IPHP_PATH . '/loader.php');
 
 // 微信支付临时配置
