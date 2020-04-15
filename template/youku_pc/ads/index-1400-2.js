4189b83d2ba8b23c548e0780edded1a24f6d25b7
pickBoutique <1781648355@qq.com>
2020-04-15 16:09:06 +0800

    //修改 pc 域名

--- template/youku_pc/ads/index-1400-2.js
+++ template/youku_pc/ads/index-1400-2.js
@@ -1,3 +1,3 @@
 document.write(
-  '<div class="yk-pannel cleafix" style="width:100%;" >\n   <div class="yk-pannel-box" >\n       <a href = "http://www.9352tv.cn" target="_blank"><img src="http://www.mzuis.com/index.php?user/publicLink&fid=1db6TcTH_clVcolnWsmSC0DFJDkBPYFmE_MqZMjOlU7yUyjDh2yotudn3x0LcTQTpfoJKCVfsmy8UMqAqhDmAR5KGvOOFPHTl2pQL2niU_GXKL-J_pbSAmUWJT8d1474Hw&file_name=/5543.png" style="width: 1200px;height: 100px;" ></a>\n   </div>\n</div>'
+  '<div class="yk-pannel cleafix" style="width:100%;" >\n   <div class="yk-pannel-box" >\n       <a href = "http://www.863my.com" target="_blank"><img src="http://www.mzuis.com/index.php?user/publicLink&fid=1db6TcTH_clVcolnWsmSC0DFJDkBPYFmE_MqZMjOlU7yUyjDh2yotudn3x0LcTQTpfoJKCVfsmy8UMqAqhDmAR5KGvOOFPHTl2pQL2niU_GXKL-J_pbSAmUWJT8d1474Hw&file_name=/5543.png" style="width: 1200px;height: 100px;" ></a>\n   </div>\n</div>'
 );
