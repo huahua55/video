7fe1a10d375741bcda4f0b8b2b52a51e305a40cc
maxiaoyu <784721293@qq.com>
2020-04-22 14:45:18 +0800

    404 错误

--- application/api/controller/MyError.php
+++ application/api/controller/MyError.php
@@ -6,9 +6,7 @@ class MyError
     public function _empty()
     {
         header("HTTP/1.0 404 Not Found");
-        echo  '<script>setTimeout(function (){location.href="'.MAC_PATH.'";},'.(2000).');</script>';
-        $msg = '页面不存在';
-        abort(404,$msg);
+        echo  '请求地址错误';
         exit;
     }
 }
