383b03b3eff00cfb438c59c1e19ba4b8a8f60ce9
pickBoutique <1781648355@qq.com>
2020-04-23 15:09:27 +0800

    //添加 update

--- application/crontab/command/CmsVodScore.php
+++ application/crontab/command/CmsVodScore.php
@@ -229,9 +229,9 @@ class CmsVodScore extends Command
 //            $vod_data['vod_blurb'] = "'$vod_content'";
 //        }
         //时长
-        if (isset($res['vod_duration'])) {
-            $vod_data['vod_duration'] = trim($res['vod_duration']);
-        }
+//        if (isset($res['vod_duration'])) {
+//            $vod_data['vod_duration'] = trim($res['vod_duration']);
+//        }
 
         //豆瓣id
         if (isset($res['vod_douban_id'])) {
