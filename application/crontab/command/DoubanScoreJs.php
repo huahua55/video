9bf5d50cf90d5ac4d9886225b284d5cf3882d409
pickBoutique <1781648355@qq.com>
2020-04-25 14:35:21 +0800

    //添加 update

--- application/crontab/command/DoubanScoreJs.php
+++ application/crontab/command/DoubanScoreJs.php
@@ -339,7 +339,7 @@ class DoubanScoreJs extends Common
         $error_data = $this->vod_errorDb->where($error_where)->find();
         if(empty($error_data)){
             log::info('js-addUpError::su--' .$douban_id);
-            if ($douban_id > 0) {
+//            if ($douban_id > 0) {
                 $deas_data['vod_id'] = $v['vod_id'];
                 $deas_data['title'] = $v['vod_name']??'';
                 $deas_data['douban_id'] = $douban_id;
@@ -354,7 +354,7 @@ class DoubanScoreJs extends Common
                 } catch (\Exception $e) {
                     log::info('js-采集豆瓣评分-err0r数据重复添加::' . $v['vod_id'].$e);
                 }
-            }
+//            }
         }else{
             log::info('js-addUpError::count' );
             if($e_err == false){
