08f08248d1fefea09706ac9682a162a1d0546cbd
pickBoutique <1781648355@qq.com>
2020-04-27 18:27:32 +0800

    //添加 update

--- application/command.php
+++ application/command.php
@@ -10,11 +10,13 @@
 // +----------------------------------------------------------------------
 
 return [
-    'app\crontab\command\Cj',
-    'app\crontab\command\DoubanScore',
-    'app\crontab\command\CmsDouban',
-    'app\crontab\command\CmsVodScore',
-    'app\crontab\command\DoubanScoreCopy',
-    'app\crontab\command\DoubanScoreJs',
-    'app\crontab\command\DoubanTopList'
+    'app\crontab\command\Cj',//采集当天资源站
+    'app\crontab\command\DoubanScore',//采集数据 cms返回反应慢
+    'app\crontab\command\CmsDouban',//更新当前豆瓣详情
+    'app\crontab\command\CmsVodScore',//合并push到数据表中
+    'app\crontab\command\DoubanScoreCopy',//采集数据接口  不稳定
+    'app\crontab\command\DoubanScoreJs',//采集数据js  目前在使用
+    'app\crontab\command\DoubanTopList',//推荐定时脚本
+    'app\crontab\command\VodCode',
+    'app\crontab\command\editVod',//修改视频表数据
 ];
