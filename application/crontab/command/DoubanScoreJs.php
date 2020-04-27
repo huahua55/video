53ee037b33c6e8c1d3f62d456c6c07170b9e4a03
pickBoutique <1781648355@qq.com>
2020-04-27 15:22:44 +0800

    //添加 update

--- application/crontab/command/DoubanScoreJs.php
+++ application/crontab/command/DoubanScoreJs.php
@@ -185,7 +185,7 @@ class DoubanScoreJs extends Common
                     unset($startTime);
                     if (empty($getSearchData)) {
                         log::info('js-采集豆瓣评分-url-err::');//更新 代理
-                        $this->update_url_proxy($error_count, $url);
+//                        $this->update_url_proxy($error_count, $url);
                     }
 
                     log::info('js-采集豆瓣评分-url-::' . $url);
