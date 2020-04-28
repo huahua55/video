8ca30b98582d24dd7523d5d22ffc939b726182c5
GE-TAO <943540932@qq.com>
2020-04-28 11:20:50 +0800

    优化分享以及title

--- template/youku_pc/youku/js/jquery.SuperSlide.js
+++ template/youku_pc/youku/js/jquery.SuperSlide.js
@@ -499,16 +499,16 @@ function addFavorite(obj, opts) {
 		}
 	}
 }
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function() {
-	if (document.hidden) {
-		document.title = '(つェ⊂) 我藏好了哦~ ' + OriginTitile;
-		clearTimeout(titleTime);
-	} else {
-		document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitile;
-		titleTime = setTimeout(function() {
-			document.title = OriginTitile;
-		}, 1000);
-	}
-});
+// var OriginTitile = document.title;
+// var titleTime;
+// document.addEventListener('visibilitychange', function() {
+// 	if (document.hidden) {
+// 		document.title = '(つェ⊂) 我藏好了哦~ ' + OriginTitile;
+// 		clearTimeout(titleTime);
+// 	} else {
+// 		document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitile;
+// 		titleTime = setTimeout(function() {
+// 			document.title = OriginTitile;
+// 		}, 1000);
+// 	}
+// });
