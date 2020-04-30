3bfc2df8c54df9f23a839c54ae6f9dc4632df788
pickBoutique <1781648355@qq.com>
2020-04-30 14:21:51 +0800

    //添加 update

--- my.php
+++ my.php
@@ -12,7 +12,7 @@ $mysql_database = 'video'; //改成自己的mysql数据库名
 $conn=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database); //连接数据库
 
 //连接数据库错误提示
-var_dump($conn);
+print_r($conn);
 if (mysqli_connect_errno($conn)) {
 
     die("连接 MySQL 失败: " . mysqli_connect_error());
