e76375bcca8488a976bb069718bb48fbee3a06cc
maxiaoyu <784721293@qq.com>
2020-05-08 11:11:10 +0800

    渠道设置

---
+++ application/admin/controller/Channel.php
@@ -0,0 +1,121 @@
+<?php
+namespace app\admin\controller;
+use think\Db;
+
+class Channel extends Base
+{
+    public function __construct(){
+        parent::__construct();
+    }
+
+
+    public function index()
+    {
+        $param = input();
+        $param['page'] = intval($param['page']) <1 ? 1 : $param['page'];
+        $param['limit'] = intval($param['limit']) <1 ? $this->_pagesize : $param['limit'];
+        $where=[];
+
+        if(!empty($param['wd'])){
+            $param['wd'] = htmlspecialchars(urldecode($param['wd']));
+            $where['name'] = ['like','%'.$param['wd'].'%'];
+        }
+
+        $order='id desc';
+        $res = model('Channel')->listData($where,$order,$param['page'],$param['limit']);
+
+        $this->assign('list',$res['list']);
+        $this->assign('total',$res['total']);
+        $this->assign('page',$res['page']);
+        $this->assign('limit',$res['limit']);
+
+
+        $param['page'] = '{page}';
+        $param['limit'] = '{limit}';
+        $this->assign('param',$param);
+        $this->assign('title','渠道配置管理');
+        return $this->fetch('admin@channel/index');
+    }
+
+    public function info(){
+        if (Request()->isPost()) {
+            $param = input();
+            $res = model('Channel')->saveData($param);
+            if($res['code']>1){
+                return $this->error($res['msg']);
+            }
+            return $this->success($res['msg']);
+        }
+
+        $id = input('id');
+        $where=[];
+        $where['id'] = ['eq',$id];
+        $res = model('Channel')->infoData($where);
+
+        $this->assign('info',$res['info']);
+
+        $this->assign('title','渠道信息');
+        return $this->fetch('admin@channel/info');
+    }
+
+    public function del()
+    {
+        $param = input();
+        $ids = $param['ids'];
+
+        if(!empty($ids)){
+            $where=[];
+            $where['id'] = ['in',$ids];
+            $res = model('Channel')->delData($where);
+            if($res['code']>1){
+                return $this->error($res['msg']);
+            }
+            return $this->success($res['msg']);
+        }
+        return $this->error('参数错误');
+    }
+
+    public function batch()
+    {
+        $param = input();
+        $ids = $param['ids'];
+        foreach ($ids as $k=>$id) {
+            $data = [];
+            $data['id'] = intval($id);
+            $data['name'] = $param['name'][$k];
+
+            $data['recom_vod'] = $param['recom_vod'][$k];
+
+            if (empty($data['name'])) {
+                $data['name'] = '未知';
+            }
+            $res = model('Channel')->saveData($data);
+            if($res['code']>1){
+                return $this->error($res['msg']);
+            }
+        }
+        $this->success($res['msg']);
+    }
+
+    public function field(){
+        $param = input();
+        $ids = $param['ids'];
+        $col = $param['col'];
+        $val = $param['val'];
+
+        if(!empty($ids) && in_array($col,['states']) && in_array($val,['0','1'])){
+            $where=[];
+            $where['id'] = ['in',$ids];
+
+            $res = model('Channel')->fieldData($where,$col,$val);
+            if($res['code']>1){
+                return $this->error($res['msg']);
+            }
+            return $this->success($res['msg']);
+        }
+        return $this->error('参数错误');
+    }
+
+
+
+}
--- application/common/model/Channel.php
+++ application/common/model/Channel.php
@@ -6,5 +6,66 @@ class Channel extends Base{
 
     protected $createTime = false;
 
+    public function listData($where,$order,$page,$limit=20)
+    {
+        $total = $this->where($where)->count();
+        $list = Db::name('Channel')->where($where)->order($order)->page($page)->limit($limit)->select();
+        return ['code'=>1,'msg'=>'数据列表','page'=>$page,'pagecount'=>ceil($total/$limit),'limit'=>$limit,'total'=>$total,'list'=>$list];
+    }
 
+    public function infoData($where=[],$field='*')
+    {
+        if(empty($where) || !is_array($where)){
+            return ['code'=>1001,'msg'=>'参数错误'];
+        }
+        $info = Db::name("Channel")->field($field)->where($where)->find();
+
+        if(empty($info)){
+            return ['code'=>1002,'msg'=>'获取数据失败'];
+        }
+
+        return ['code'=>1,'msg'=>'获取成功','info'=>$info];
+    }
+
+    public function saveData($data)
+    {
+        $data['create_time'] = time();
+        if(!empty($data['id'])){
+            $where=[];
+            $where['id'] = ['eq',$data['id']];
+
+            $res = Db::name('Channel')->where($where)->update($data);
+        }
+        else{
+            $res = Db::name('Channel')->insert($data);
+        }
+        if(false === $res){
+            return ['code'=>1002,'msg'=>'保存失败：'.$this->getError() ];
+        }
+        return ['code'=>1,'msg'=>'保存成功'];
+    }
+
+    public function delData($where){
+        //删除content
+        $res = Db::name('Channel')->where($where)->delete();
+
+        if($res===false){
+            return ['code'=>1001,'msg'=>'删除失败'.$this->getError() ];
+        }
+        return ['code'=>1,'msg'=>'删除成功'];
+    }
+
+    public function fieldData($where,$col,$val){
+        if(!isset($col) || !isset($val)){
+            return ['code'=>1001,'msg'=>'参数错误'];
+        }
+
+        $data = [];
+        $data[$col] = $val;
+        $res = $this->allowField(true)->where($where)->update($data);
+        if($res===false){
+            return ['code'=>1001,'msg'=>'设置失败：'.$this->getError() ];
+        }
+        return ['code'=>1,'msg'=>'设置成功'];
+    }
 }
