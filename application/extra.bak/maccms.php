4781fe9e8030e58c434e257a05d397ce4e2a708c
pickBoutique <1781648355@qq.com>
2020-05-11 17:45:03 +0800

    //添加 update

--- application/extra.bak/maccms.php
+++ application/extra.bak/maccms.php
@@ -137,8 +137,9 @@ return array (
     'watermark_color' => '#FF0000',
     'protocol' => 'http',
     'mode' => 'remote',
-    'remoteurl' => 'http://s1.863my.com/',
-    'api' => 
+    'remoteurl' => 'http://www.863my.com/',
+//    'remoteurl' => 'http://s1.863my.com/',
+    'api' =>
     array (
       'upyun' => 
       array (
