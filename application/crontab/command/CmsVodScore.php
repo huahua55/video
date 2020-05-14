379d258736868e3a8f222a52dd180d1a2bfe6f70
pickBoutique <1781648355@qq.com>
2020-05-14 19:25:36 +0800

    //添加 update

--- application/crontab/command/CmsVodScore.php
+++ application/crontab/command/CmsVodScore.php
@@ -177,6 +177,7 @@ class CmsVodScore extends Common
     protected function getVodDoubanScoreData($where, $order, $page, $limit, $start)
     {
         $limit_str = ($limit * ($page - 1) + $start) . "," . $limit;
+
         $total = $this->vodDb->where($where)->count();
         $list = $this->vodDb->field('vod_id,vod_sub,vod_year,vod_name,vod_class,vod_actor,vod_director,vod_douban_id,vod_douban_score')->where($where)->order($order)->limit($limit_str)->select();
         return ['pagecount' => ceil($total / $limit), 'list' => $list];
