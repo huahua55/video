304f22d282c921ccca5cd41894c7cfcff03a4d28
pickBoutique <1781648355@qq.com>
2020-05-19 14:36:28 +0800

    //添加 update

--- application/crontab/command/CjUpdateTime.php
+++ application/crontab/command/CjUpdateTime.php
@@ -43,7 +43,7 @@ class CjUpdateTime extends Command
 
         $where = [];
         $where['vod_douban_id'] = ['neq', 3];
-        $where['vod_id'] = ['eq', 984];
+//        $where['vod_id'] = ['eq', 984];
 //        $startTime =  date("Y-m-d 00:00:00",time());
 //        $endTime =  date("Y-m-d 23:59:59",time());
 //        $where['vod_time'] =['between',[strtotime($startTime),strtotime($endTime)]];
