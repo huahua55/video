a08a25cd545965f5dae3561521f7980fb28b4e41
pickBoutique <1781648355@qq.com>
2020-07-23 09:05:27 +0800

    // mysql

--- my.php
+++ my.php
@@ -1,18 +1,18 @@
 <?php
 
-print_r(phpinfo());die;
-$mysql_server_name = '127.0.0.1'; //改成自己的mysql数据库服务器
+
+$mysql_server_name = 'rm-j6c57u1v6upihhwqq.mysql.rds.aliyuncs.com'; //改成自己的mysql数据库服务器
+//$mysql_server_name = 'rm-j6c57u1v6upihhwqq6o.mysql.rds.aliyuncs.com'; //改成自己的mysql数据库服务器
 
 $mysql_username = 'uservide'; //改成自己的mysql数据库用户名
 
-$mysql_password = 'CHjduTY79&3CKLp'; //改成自己的mysql数据库密码
+$mysql_password = 'CHjduTY793CKLp'; //改成自己的mysql数据库密码
 
 $mysql_database = 'video'; //改成自己的mysql数据库名
 
 $conn=mysqli_connect($mysql_server_name,$mysql_username,$mysql_password,$mysql_database); //连接数据库
 
 //连接数据库错误提示
-print_r($conn);
 if (mysqli_connect_errno($conn)) {
 
     die("连接 MySQL 失败: " . mysqli_connect_error());
@@ -27,13 +27,12 @@ mysqli_query($conn,"set names utf8"); //数据库编码格式
 
 //查询代码
 
-$sql = "select * from art";
+$sql = "SELECT `a`.`vod_name`,b.id as b_id,b.examine_id as b_examine_id,b.sum as b_sum,b.is_examine as b_is_examine,b.is_section as b_is_section,b.reason as b_reason,b.code as b_code,b.vod_id as b_vod_id,b.video_id as b_video_id,b.down_ts_url as b_down_ts_url,b.down_mp4_url as b_down_mp4_url,b.down_url as b_down_url,b.down_time as b_down_time,b.weight as b_weight,b.is_down as b_is_down,b.is_sync as b_is_sync FROM `video_vod` `b` LEFT JOIN `vod` `a` ON `b`.`vod_id`=`a`.`vod_id` WHERE  INSTR(`a`.`vod_name`,'张三') > 0  OR `b`.`id` = '张三' ORDER BY `b`.`weight` DESC,`b`.`down_time` DESC LIMIT 0,10";
 
 $query = mysqli_query($conn,$sql);
-
 while($row = mysqli_fetch_array($query)){
-
-    echo $row['art_id'];
+    print_r($row[1]??'');
+    print_r($row['vod_name']??'');
 
 }
 
