f03f8470db433a48d9f2c42457f7338482e9a5ab
pickBoutique <1781648355@qq.com>
2020-07-24 20:17:21 +0800

    // update 不杀死进程

--- deploy/crontab/iVideoVod.sh
+++ deploy/crontab/iVideoVod.sh
@@ -3,5 +3,11 @@ PATH=/usr/local/php/bin:/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbi
 # 1.执行 php 命令不需要到thinkphp项目的目录下 2.index.php为入口文件 3.第三个参数为需要执行方法的路由
 # 当天
 cd /data/www/video/
-ps -ef | grep pushData | grep -v grep | awk '{print $2}' | xargs kill -9
-php think pushData name=i
+a=`ps -ef | grep pushData |grep name= | grep -v grep | awk '{print $2}'`
+if [ ! -n "$a" ]; then
+  #空的
+  php think pushData name=i
+else
+  # 不是空的 先不杀死
+#  ps -ef | grep pushData |grep name=i | grep -v grep | awk '{print $2}' | xargs kill -9
+fi
