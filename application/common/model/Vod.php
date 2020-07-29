f2f9acbbacdff1900f8e47fed1b7353d023b1098
liusongwei <liusongwei@taodou.com>
2020-07-29 21:15:01 +0800

    同步任务表数据bug修改

--- application/common/model/Vod.php
+++ application/common/model/Vod.php
@@ -695,15 +695,21 @@ class Vod extends Base {
             /***** 这里修改数据同步到video、video_vod表 *****/
 
             // 根据vod_id去任务表video_vod查找关联的video表的video_id
-            $video_id = model('video_vod')->where( $where )->column('video_id');
+            $video_vod_where['vod_id'] = ['eq',$data['vod_id']];
+            $video_vod_where['video_id'] = ['NEQ',0];
+            $video_id = Db::name('video_vod')->field('id,video_id')->where( $video_vod_where )->find();
+
             if (!empty( $video_id )) {
 
-                $data['up_time'] = time();
-                $save_vedio_vod = model('video_vod')->allowField(true)->where( ['video_id' => $video_id[0]] )->update( $data );
+                $save_vedio_vod_edit['up_time'] = time();
+                $save_vedio_vod_edit['type_id_1'] = $data['type_id_1'];
+                $save_vedio_vod_edit['type_id'] = $data['type_id'];
+                $save_vedio_vod_edit['vod_name'] = $data['vod_name'];
+                $save_vedio_vod = model('video_vod')->allowField(true)->where( ['id'=>$video_id['id']] )->update( $save_vedio_vod_edit );
                 unset( $data['up_time'] );
 
                 $data['type_pid'] = $data['type_id_1'];
-                $save_video = model('video')->allowField(true)->where( ['id' => $video_id[0]] )->update( $data );
+                $save_video = model('video')->allowField(true)->where( ['id' => $video_id['video_id']] )->update( $data );
                 unset( $data['type_pid'] );
             }
 
