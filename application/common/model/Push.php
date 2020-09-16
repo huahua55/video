c99fbb03fe3bf9bfae56b68c3751ce02dfce34bd
pickBoutique <1781648355@qq.com>
2020-09-16 09:01:15 +0800

    //修复bug

--- application/common/model/Push.php
+++ application/common/model/Push.php
@@ -295,13 +295,14 @@ class Push extends Base {
         if (!empty( $new_url['vod_name'])){
             $find_records = $this->find_records();
             foreach ($find_records as $find_records_key => $find_records_val){
-                $count3 = substr_count($find_records_val, $new_url['vod_name']);
+                $count3 = substr_count( $new_url['vod_name'],$find_records_val);
                 if ($count3 > 0) {
                     $new_url['weight'] = 99;
                     break;
                 }
             }
         }
+//        p($new_url);
 //        $new_url['weight'] = $v['vod_douban_score'] ?? '0';
         $new_url['down_url'] = $new_down_url[$k_p_play]['down_url'] ?? '';
         $new_url['m3u8_url'] = $k_p_val['m3u8_url'] ?? '';
