5db1676c8f8a55c5858fa7b15f9482c93cb8c319
pickBoutique <1781648355@qq.com>
2020-09-30 11:03:43 +0800

    //修复bug

--- deploy/crontab/cj_zd.sh
+++ deploy/crontab/cj_zd.sh
@@ -3,10 +3,11 @@ PATH=/usr/local/php/bin:/opt/someApp/bin:/usr/local/sbin:/usr/local/bin:/usr/sbi
 # 1.执行 php 命令不需要到thinkphp项目的目录下 2.index.php为入口文件 3.第三个参数为需要执行方法的路由
 # 当天
 
-cd /data/www/video/
-chown -R www:www ../video
+#cd /data/www/video/
+#chown -R www:www ../video
 time1=$(date "+%M")
-if [ "$time1" = 31 ] || [ "$time1" = 01 ];then
+echo $time1
+if [ "$time1" = 30 ] || [ "$time1" = 00 ];then
   sleep 5
   php think Cj name=zuidacj
 fi
