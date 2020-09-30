c0bfabddf2c929cd5b5bf656d440c3a5b4f59333
pickBoutique <1781648355@qq.com>
2020-09-30 09:53:45 +0800

    //修复bug

--- deploy/crontab/cj_zd.sh
+++ deploy/crontab/cj_zd.sh
@@ -9,7 +9,7 @@ chown -R www:www ../video
 time1=$(date "+%M")
 len_time=${#time1}
 if ((len_time==2));then
-  if [ "$time1" = 30 ] || [ "$time1" = 00 ];then
+  if [ "$time1" = 31 ] || [ "$time1" = 01 ];then
     php think Cj name=zuidacj
   fi
 fi
