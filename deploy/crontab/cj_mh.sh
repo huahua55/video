2f576efc1447f8f53412cd3e94949a8e2dc80460
pickBoutique <1781648355@qq.com>
2020-10-30 09:35:23 +0800

    //修复bug

--- deploy/crontab/cj_mh.sh
+++ deploy/crontab/cj_mh.sh
@@ -7,18 +7,18 @@ cd /data/www/video/
 chown -R www:www ../video
 
 
-#time1=$(date "+%M")
-#len_time=${#time1}
-#if ((len_time==2));then
-#  time1_str=${time1:1}
-#else
-#  time1_str=${time1:0:1}
-#fi
-#if (($time1_str!=5));then
-#  # 不存在 5 的时候跑麻花
-#  sleep 3
-#  php think Cj name=mhysday
-#else
-#  # 暂不处理
-#  echo $time1_str
-#fi
+time1=$(date "+%M")
+len_time=${#time1}
+if ((len_time==2));then
+  time1_str=${time1:1}
+else
+  time1_str=${time1:0:1}
+fi
+if (($time1_str!=5));then
+  # 不存在 5 的时候跑麻花
+  sleep 6
+  php think Cj name=mhysday
+else
+  # 暂不处理
+  echo $time1_str
+fi
